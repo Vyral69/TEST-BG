a7af7ff970ee8d2974fa3d2c64310a9cd9234295
Vyral69 <159704030+Vyral69@users.noreply.github.com>
2024-11-12 00:09:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Aan het kijken naar DenZuider"];
+const statusMessages = ["Aan het kijken naar 👀 DenZuider"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
